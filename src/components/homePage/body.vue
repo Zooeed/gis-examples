e213a44a176beda9bca55a3e0cd0f1f45bfb4749
jiepengrui <17600519219@163.com>
2024-10-24 17:41:34 +0800

    页面划分

--- src/components/homePage/body.vue
+++ src/components/homePage/body.vue
@@ -1,90 +1,62 @@
 <template>
     <div class="page_body">
-            <el-anchor :container="containerRef" style="overflow-y: auto" direction="vertical" type="default" :offset="30" @click="handleClick">
-                <el-tree :data="dataSource"  node-key="id" default-expand-all :expand-on-click-node="false">
-                    <template #default="{ node, data }">
-                        <span class="custom-tree-node">
-                            <el-anchor-link :href="'#'+data.id" :title="data.id" />
-                        </span>
-                    </template>
-                </el-tree>
-            </el-anchor>
-       
-            <div ref="containerRef" style="height: 300px;overflow-y:auto;">
-                <div v-for="item in dataSource" :id="item.id" style="
-                    height: 300px;
+        <el-anchor :container="containerRef" style="overflow-y: auto" direction="vertical" type="default" :offset="30"
+            @click="handleClick">
+            <el-tree :data="dataSource" node-key="id" default-expand-all :expand-on-click-node="false">
+                <template #default="{ node, data }">
+                    <span class="custom-tree-node">
+                        <el-anchor-link :href="'#part' + data.id" :title="'part' + data.id" />
+                    </span>
+                </template>
+            </el-tree>
+        </el-anchor>
+
+        <div class="views" ref="containerRef" style="overflow-y:auto;">
+            <div v-for="item in dataSource" :id="'part' + item.id" style="
+                    height: auto;
                     background: rgba(255, 0, 0, 0.02);
                     margin-top: 30px;
                 ">
-                    {{ item.label }}
-                    <div v-for="child in item?.children2" class="itemview" :id="child.id" style="width: 300px;height: 300px;">
-                        {{ child.label }}
-                    </div>
+                {{ 'part' + item.label }}
+                <div v-for="child in item?.children" class="itemview" :id="'part' + child.id"
+                    style="width: 300px;height: 300px;">
+                    {{ 'part' + child.id }}
                 </div>
             </div>
+        </div>
     </div>
 </template>
 
 
 
 <script setup>
-import { ref , onMounted} from 'vue'
+import { ref, onMounted } from 'vue'
+import pageConfig from './pageConfig.js'
 
 const containerRef = ref();
 
-let dataSource = ref([
-    {
-        id: "1",
-        label: 'Level one 1',
-        // children: [
-        //     {
-        //         id: 4,
-        //         label: 'Level two 1-1',
-        //     },
-        // ],
-    },
-    {
-        id: "2",
-        label: 'Level one 2',
-        // children: [
-        //     {
-        //         id: 5,
-        //         label: 'Level two 2-1',
-        //     },
-        //     {
-        //         id: 6,
-        //         label: 'Level two 2-2',
-        //     },
-        // ],
-    },
-    {
-        id: "3",
-        label: 'Level one 3',
-        // children: [
-        //     {
-        //         id: 7,
-        //         label: 'Level two 3-1',
-        //     },
-        //     {
-        //         id: 8,
-        //         label: 'Level two 3-2',
-        //     },
-        // ],
-    },
-])
+let dataSource = ref(pageConfig)
 const handleClick = (e) => {
     e.preventDefault()
 }
 
 onMounted(() => {
-  console.log(containerRef); 
+    console.log(containerRef);
 });
 </script>
 
 <style lang="scss" scoped>
-.page_body{
+.page_body {
     display: grid;
     grid-template-columns: 3fr 9fr;
     grid-template-rows: 1fr;
+    overflow-x: hidden;
+
+    .views::-webkit-scrollbar-thumb {
+        /*滚动条里面小方块*/
+        border-radius: 10px;
+        height: 50px;
+        -webkit-box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
+    }
 }
 </style>
